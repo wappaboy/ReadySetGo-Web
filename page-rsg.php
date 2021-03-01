f92e112686ecd71fe0cfce35a64bfb1451fb1aa8
HomePC/DesktopApp <shogo.mtsk@gmail.com>
2021-03-02 08:42:43 +0900

    edit staff

---
+++ page-rsg.php
@@ -0,0 +1,13 @@
+﻿<?php
+/*
+Template Name: Ready? Set Go! テンプレート
+*/
+?>
+<h1>固定ページの新規テンプレートです。</h1>
+<?php
+if(have_posts()): while(have_posts()): the_post();?>
+<h2><?php the_title(); ?></h2>
+
+<?php the_content(); ?>
+
+<?php endwhile; endif; ?>
